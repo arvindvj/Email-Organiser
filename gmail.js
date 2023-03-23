33791e593304343a4fc4acc91a4899195a5d0f63
Arvind Vidyashankar <arvindvj@users.noreply.github.com>
2023-03-23 13:30:03 +0000

    Add Label based on sender email or keyword in subject

---
+++ gmail.js
@@ -0,0 +1,45 @@
+function organizeEmails() {
+  // Define the rules for email organization
+  const rules = [
+    { keyword: 'promotion', label: 'Promotions' },
+    { sender: 'example@example.com', label: 'Sender Label' },
+  ];
+
+  // Search for emails in the inbox
+  const inbox = GmailApp.getInboxThreads();
+
+  // Iterate through each email
+  for (const thread of inbox) {
+    // Get the subject and sender of the email
+    const subject = thread.getFirstMessageSubject().toLowerCase();
+    const sender = thread.getMessages()[0].getFrom().toLowerCase();
+
+    // Initialize a variable to track whether a rule has been applied
+    let ruleApplied = false;
+
+    // Apply rules based on keywords
+    for (const rule of rules) {
+      if (rule.keyword && subject.includes(rule.keyword) || rule.sender && sender.includes(rule.sender)) {
+        // Create the label if it doesn't exist
+        let label = GmailApp.getUserLabelByName(rule.label);
+        if (!label) {
+          label = GmailApp.createLabel(rule.label);
+        }
+
+        // Apply the label to the email thread
+        thread.addLabel(label);
+
+        // Set ruleApplied to true, since a rule has been applied
+        ruleApplied = true;
+
+        // Stop checking other rules for this email
+        break;
+      }
+    }
+
+    // (Optional) Archive the email thread if a rule has been applied
+    if (ruleApplied) {
+      // thread.moveToArchive();
+    }
+  }
+}
